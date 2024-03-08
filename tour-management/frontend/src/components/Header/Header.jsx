98619a9f61a614bc3523863dbf22808b366522f1
Seeker577 <mydolpin555@gmail.com>
2024-03-08 17:51:12 +0530

    Updated

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const nav__links = [
     display: 'Home'
   },
   {
-    path:'#',
+    path:'/about',
     display:'About'
   },
   {
@@ -39,7 +39,7 @@ const Header = () => {
             <ul className="menu d-flex align-items-center gap-5">
               {nav__links.map((item, index) =>(
                 <li className='nav_item' key = {index}>
-                  <NavLink to={item.path}>
+                  <NavLink to={item.path} className={navClass=> navClass.isActive ? "active__link" : ""}>
                     {item.display}
                   </NavLink>
                 </li>
--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import "../styles/home.css"
+
+import {Container, Row, Col} from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from './../shared/subtitle';
+
+import SearchBar from '../shared/SearchBar';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+  {/* =========== hero section starts ===========  */}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+          <div className="hero__content">
+            <div className='hero__subtitle d-flex align-items-center'>
+              <Subtitle subtitle={'Know Before You Go'}/>
+              <img src={worldImg} alt="" />
+            </div>
+            <h1>Traveling opens the door to creating <spanc
+            className='highlight'>memories</spanc></h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero facilis dignissimos itaque omnis fugiat dolores vitae saepe facere ad, debitis quas esse deserunt explicabo, eius blanditiis ipsam sed. Quod, ratione!</p>
+          </div>
+        </Col>
+
+        <Col lg='2'>
+          <div className='hero__img-box'>
+            <img src={heroImg} alt=""/>
+          </div>
+        </Col>
+        <Col lg='2'>
+          <div className='hero__img-box mt-4'>
+            <video src={heroVideo} alt="" controls/>
+          </div>
+        </Col>
+        <Col lg='2'>
+          <div className='hero__img-box mt-5'>
+            <img src={heroImg02} alt="" />
+          </div>
+        </Col>
+
+        <SearchBar />
+      </Row>
+    </Container>
+  </section>
+  {/* =========== hero section ends ===========  */}
+
+
+  </>
 }
 
 export default Home
