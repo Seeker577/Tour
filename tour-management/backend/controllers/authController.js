86a4d735b8fd0b6c9c49f4c005400ae92559d15b
Seeker577 <mydolpin555@gmail.com>
2024-03-12 00:03:48 +0530

    Updated

--- tour-management/backend/controllers/authController.js
+++ tour-management/backend/controllers/authController.js
@@ -1,22 +1,26 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
+import { MongoBatchReExecutionError } from "mongodb"
 
 
 //user registration
 export const register = async (req, res) => {
     try{
 
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(req.body.password, salt)
+
         const newUser = new User({
             username: req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-            photo:req.body.photo
+            email: req.body.email,
+            password: hash,
+            photo: req.body.photo
         })
 
         await newUser.save()
 
-        res.staus(200).json({ success:true, message: "Successfully created" })
+        res.status(200).json({ success:true, message: "Successfully created" })
 
     } catch(err) {
         res
@@ -27,6 +31,40 @@ export const register = async (req, res) => {
 
 //user login
 export const login = async (req,res) => {
+    const email = req.body.email
+
     try{
-    } catch(err) {}
+        const user = await user.findOne({email})
+
+        //if user doesn't exist
+        if(!user){
+        return res.status(404).json({success:false, message:'User not found'})
+        }
+    
+    //if user is exist the check the password or compare the password
+    const checkCorrectPassword = bcrypt.compare(req.body.password, user.password)
+
+    //if password is incorrect 
+    if(!checkCorrectPassword){
+        return res.status(401).json({success:false, message:'Incorrect email or password'})
+    }
+
+    const {password, role, ...rest} = user._doc
+
+    //create jwt token
+    const token = jwt.sign({id:user._id, role}, process.env, JWT_SECRET_KEY, { expiresIn : "15d" })
+
+    //set tooken in the browser cookies and send the response to the client
+    res.cookie("accessToken",token, {
+        httpOnly: true,
+        expires: token,expiresIn,
+
+    })    
+    .status(200)
+    .json({
+        success: true,
+        message: "successfully login",
+        data: { ...rest },
+    })
+    }catch(err) {}
 }
